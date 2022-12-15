a718760424d8a4151c4addd4eb0358098fb3f643
Mikkel Bach Nielsen <minielsen@microsoft.com>
2022-12-15 10:25:02 +0100

    updated version, add global readme option

--- demos/Program.cs
+++ demos/Program.cs
@@ -1,11 +1,16 @@
 ﻿using demos.Commands.AzureUserRootCommand;
 using demos.Commands.DaprRootCommand;
 using demos.Commands.DebugRootCommand;
+using demos.Helpers;
 using Spectre.Console;
 using System.CommandLine;
 
 
 var rootCmd = new RootCommand("A variaty of different demos");
+
+var showHelp = new Option<bool>(
+    name: "--readme", description: "Open browser and shows demos Tool readme");
+rootCmd.AddGlobalOption(showHelp);
 
 var showFiglets = new Option<bool>(
     name: "--show", description: "Show CSU Figlets");
@@ -16,17 +21,22 @@ rootCmd.AddCommand(new AzureUserCommand());
 rootCmd.AddCommand(new DaprRootCommand());
 rootCmd.AddCommand(new DebugCommand());
 
-rootCmd.SetHandler((show) =>
+rootCmd.SetHandler(async (showFiglet, showHelp) =>
 {
-    DisplayFiglets(show);
-}, showFiglets);
+    await DisplayInfo(showFiglet, showHelp);
+}, showFiglets, showHelp);
 
-void DisplayFiglets(bool show)
+async Task DisplayInfo(bool show, bool showHelp)
 {
     if(show)
     {
         AnsiConsole.Write(new FigletText(FigletFont.Load($"{AppDomain.CurrentDomain.BaseDirectory}starwars.flf"), "DK CSU").Color(Color.Red));
         AnsiConsole.Write(new FigletText(FigletFont.Load($"{AppDomain.CurrentDomain.BaseDirectory}starwars.flf"), "App Inno").Color(Color.Blue));
+    }
+    if(showHelp)
+    {
+        var cmd = $"start msedge https://github.com/mbn-ms-dk/DaprTool/blob/main/demos/documentation/demos.md";
+        await Utils.RunCmd(cmd);
     }
 }
 
